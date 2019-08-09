7ebf57617c069489359d45a16102804cf270deb1
william341 <wwokwilliam@gmail.com>
2019-08-09 17:27:17 +0000

    add more stubs

--- server/permissionhandler.go
+++ server/permissionhandler.go
@@ -6,20 +6,35 @@ func (p *Plugin) getUserPermission(userid string, permission string, teamID stri
 }
 
 //only gets user perms, not role perms
-func (p *Plugin) getUserPermissionUO(userID string, permission string, teamID string) {
-
+func (p *Plugin) getUserPermissionUO(userID string, permission string, teamID string) (hasPermission bool) {
+    //TODO: implement getting user perms
+	return p.getDefaultPermssion(permission)
 }
 
-func (p *Plugin) getUserPermissions(userID string, teamID string) {
-
+func (p *Plugin) getUserPermissions(userID string, teamID string) (permissions map[string]bool){
+	//TODO: implement getting user perms
+	m := make(map[string]bool)
+	m["send"] = getDefaultPermssion("send");
+	m["files"] = getDefaultPermssion("files");
+	m["mute"] = getDefaultPermssion("mute");
+	m["kick"] = getDefaultPermssion("kick");
+	m["ban"] = getDefaultPermssion("ban");
+	return m
 }
 
-func (p *Plugin) getUserPermissionsUO(userID string, teamID string) {
-
+func (p *Plugin) getUserPermissionsUO(userID string, teamID string) (permissions map[string]bool){
+	//TODO: implement getting user perms
+	m := make(map[string]bool)
+	m["send"] = getDefaultPermssion("send");
+	m["files"] = getDefaultPermssion("files");
+	m["mute"] = getDefaultPermssion("mute");
+	m["kick"] = getDefaultPermssion("kick");
+	m["ban"] = getDefaultPermssion("ban");
+	return m
 }
 
 func (p *Plugin) setUserPermission(userID string, teamID string, permission string, value bool) {
-
+	//TODO: implement setting user perms
 }
 
 func (p *Plugin) getDefaultPermssion(permission string) (hasPermission bool) {
@@ -34,26 +49,38 @@ func (p *Plugin) getDefaultPermssion(permission string) (hasPermission bool) {
 	}
 }
 
-func (p *Plugin) getRolePermission(roleName string, teamID string, permission string) {
+func (p *Plugin) getRolePermission(roleName string, teamID string, permission string) (hasPermission bool){
+    //TODO: implement getting role perms
+	return p.getDefaultPermssion(permission)
+}
 
+func (p *Plugin) getRolePermissions(roleName string, teamID string) (permissions map[string]bool){
+	//TODO: implement getting role perms
+	m := make(map[string]bool)
+	m["send"] = getDefaultPermssion("send");
+	m["files"] = getDefaultPermssion("files");
+	m["mute"] = getDefaultPermssion("mute");
+	m["kick"] = getDefaultPermssion("kick");
+	m["ban"] = getDefaultPermssion("ban");
+	return m
 }
 
 func (p *Plugin) setRolePermission(roleName string, teamID string, permission string, value bool) {
-
+	//TODO: implement setting role perms
 }
 
 func (p *Plugin) addUserToRole(roleName string, teamID string, userID string) {
-
+	//TODO: implement adding user to role
 }
 
 func (p *Plugin) removeUserFromRole(roleName string, teamID string, userID string) {
-
+	//TODO: implement removing user from role
 }
 
 func (p *Plugin) createRole(roleName string, teamID string) {
-
+	//TODO: implement creating a role
 }
 
 func (p *Plugin) destroyRole(roleName string, teamID string) {
-
+	//TODO: implement destroying roles
 }
