ef865e3f5c209022b999572e83fadee368243735
william341 <wwokwilliam@gmail.com>
2019-08-21 20:28:01 -0700

    msgwillbeposted

--- server/plugin.go
+++ server/plugin.go
@@ -68,18 +68,35 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 
 //MessageWillBePosted handles the message will be posted event.
 func (p *Plugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*model.Post, string) {
-	//var targetchannel *model.Channel
-	//var err2 *model.AppError
-	//targetchannel, err2 = p.API.GetChannel(post.ChannelId)
-	//if err2 != nil {
-	//	return nil, "An error has occured determining if the file could be uploaded or not. (Error determing target channel)"
-	//}
-	//var targetuser *model.User
-	//targetuser, err2 = p.API.GetUser(post.UserId)
-	//if err2 != nil {
-	//	return nil, "An error has occured determining if the file could be uploaded or not. (Error determing target user)"
-	//}
-	//targetchannel.TeamId
+	var targetchannel *model.Channel
+	var err2 *model.AppError
+	targetchannel, err2 = p.API.GetChannel(post.ChannelId)
+	if err2 != nil {
+		return nil, "An error has occured determining if the file could be uploaded or not. (Error determing target channel)"
+	}
+	var targetuser *model.User
+	targetuser, err2 = p.API.GetUser(post.UserId)
+	if err2 != nil {
+		return nil, "An error has occured determining if the file could be uploaded or not. (Error determing target user)"
+	}
+
+	if !p.getUserPermission(post.UserId, "send", targetchannel.TeamId) {
+		p.API.SendEphemeralPost(post.UserId, &model.Post{
+			ChannelId: post.ChannelId,
+			Message:   "You don't have permission to send messages.",
+		})
+		return nil, "plugin.message_will_be_posted.dismiss_post"
+	}
+
+	if !p.getUserPermission(post.UserId, "files", targetchannel.TeamId) {
+		if len(post.FileIds) != 0 {
+			p.API.SendEphemeralPost(post.UserId, &model.Post{
+				ChannelId: post.ChannelId,
+				Message:   "You don't have permission to upload messages.",
+			})
+			return nil, "plugin.message_will_be_posted.dismiss_post"
+		}
+	}
 
 	return post, ""
 }
